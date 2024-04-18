0a142f4604abdc4d4e132893b8f68d1197e3c37a
22020922 <163586571+22020922@users.noreply.github.com>
2024-04-18 20:53:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,19 +2,33 @@
 
 using namespace std;
 
-int main(){
+int main() {
+    int height = 0;
+    int looks = 0;
 
-int a;
-cout << " Input a number " << endl;
-cin >> a;
+    cout << " Enter your looks (1-10) " << endl ;
+    cin >> looks;
 
-if(a < 0){
-cout << a << " is negative " << endl;
-}else if(a == 0){
-cout << a << " is zero " << endl;
-}else{
-cout << a << " is postive " << endl;
-}
+    cout << " Enter your height (cm) " << endl;
+    cin >> height;
 
-return 0;
+    if (looks > 6) {
+        if (height < 180) {
+            cout << "You stand a chance, let's go out" << endl;
+        } else {
+            cout << "Try someone else" << endl;
+        }
+    } else if (looks <= 6) {
+        if (height >= 180) {
+            cout << "You don't look the part and you're also too tall" << endl;
+        } else if (height < 165) {
+            cout << " You don't look the part and you're too short" << endl;
+        } else {
+            cout << "You don't look the part" << endl;
+        }
+    } else {
+        cout << "Please enter valid information" << endl;
+    }
+
+    return 0;
 }
